5995ff9a4fecdcc39ae1c39b11f6292efbad8b2f
Mike Devresse <mike.devresse@gmail.com>
2022-01-25 16:36:26 +0100

    Frontend: better statistics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,10 @@ class App extends Component {
         this.state = {
             commandHistory: [],
             resultHistory: [],
-            slaves: 0,
-            queue: 0
+            slavesAvailable: 0,
+            slavesWorking: 0,
+            queue: 0,
+            searching: 0
         }
     }
 
@@ -22,17 +24,21 @@ class App extends Component {
                 let state = {
                     commandHistory: [...this.state.commandHistory, {msg: msg.data, date: (new Date()).toLocaleString()}],
                     resultHistory: this.state.resultHistory,
-                    slaves: this.state.slaves,
-                    queue: this.state.queue
+                    slavesAvailable: this.state.slavesAvailable,
+                    slavesWorking: this.state.slavesWorking,
+                    queue: this.state.queue,
+                    searching: this.state.searching
                 }
                 if(msgSplit[0] === "found" && msgSplit.length === 3) {
                     state.resultHistory = [...this.state.resultHistory, {hash: msgSplit[1], result: msgSplit[2]}]
                 }
-                if(msgSplit[0] === "slaves" && msgSplit.length === 2) {
-                    state.slaves = msgSplit[1]
+                if(msgSplit[0] === "slaves" && msgSplit.length === 3) {
+                    state.slavesAvailable = msgSplit[1]
+                    state.slavesWorking = msgSplit[2]
                 }
-                if(msgSplit[0] === "queue" && msgSplit.length === 2) {
+                if(msgSplit[0] === "queue" && msgSplit.length === 3) {
                     state.queue = msgSplit[1]
+                    state.searching = msgSplit[2]
                 }
 
                 return state
--- frontend/src/components/CommandPanel/Monitor/Monitor.jsx
+++ frontend/src/components/CommandPanel/Monitor/Monitor.jsx
@@ -9,14 +9,24 @@ class Monitor extends Component {
                <Card>
                    <Card.Header>Monitoring</Card.Header>
                    <Table className="mb-0">
+                       <tbody>
                        <tr>
-                           <td>Slaves</td>
-                           <td>{this.props.state.slaves}</td>
+                           <td>Slaves available</td>
+                           <td>{this.props.state.slavesAvailable}</td>
+                       </tr>
+                       <tr>
+                           <td>Slaves working</td>
+                           <td>{this.props.state.slavesWorking}</td>
                        </tr>
                        <tr>
                            <td>Queue</td>
                            <td>{this.props.state.queue}</td>
                        </tr>
+                       <tr>
+                           <td>Searching</td>
+                           <td>{this.props.state.searching}</td>
+                       </tr>
+                       </tbody>
                    </Table>
                </Card>
             </Col>
