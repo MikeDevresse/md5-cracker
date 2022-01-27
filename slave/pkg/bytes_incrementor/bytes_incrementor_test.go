416ac4179ca78f840b9c43c22ff4c19ddb5c37bb
Mike Devresse <mike.devresse@gmail.com>
2022-01-27 09:13:23 +0100

    Slave: fix tests

--- slave/pkg/bytes_incrementor/bytes_incrementor_test.go
+++ slave/pkg/bytes_incrementor/bytes_incrementor_test.go
@@ -6,32 +6,38 @@ import (
 )
 
 func TestIncrement(t *testing.T) {
-	got := Increment([]byte("a"), 0)
+	got := []byte("a")
+	Increment(&got, 0)
 	if !bytes.Equal(got, []byte("b")) {
 		t.Errorf("Increment([]byte(\"a\"), 0) = %v, want %v", got, []byte("b"))
 	}
 
-	got = Increment([]byte("z"), 0)
+	got = []byte("z")
+	Increment(&got, 0)
 	if !bytes.Equal(got, []byte("A")) {
 		t.Errorf("Increment([]byte(\"z\"), 0) = %v, want %v", got, []byte("A"))
 	}
 
-	got = Increment([]byte("Z"), 0)
+	got = []byte("Z")
+	Increment(&got, 0)
 	if !bytes.Equal(got, []byte("0")) {
 		t.Errorf("Increment([]byte(\"Z\"), 0) = %v, want %v", got, []byte("0"))
 	}
 
-	got = Increment([]byte("a9"), 1)
+	got = []byte("a9")
+	Increment(&got, 1)
 	if !bytes.Equal(got, []byte("ba")) {
 		t.Errorf("Increment([]byte(\"a9\"), 0) = %v, want %v", got, []byte("a9"))
 	}
 
-	got = Increment([]byte("8999"), 3)
+	got = []byte("8999")
+	Increment(&got, 3)
 	if !bytes.Equal(got, []byte("9aaa")) {
 		t.Errorf("Increment([]byte(\"8999\"), 0) = %v, want %v", got, []byte("9aaa"))
 	}
 
-	got = Increment([]byte("9"), 0)
+	got = []byte("9")
+	Increment(&got, 0)
 	if !bytes.Equal(got, []byte("aa")) {
 		t.Errorf("Increment([]byte(\"9\"), 0) = %v, want %v", got, []byte("aa"))
 	}
